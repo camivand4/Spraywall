56b1ab055cc420e761bd2b46a915a4cab0c67e39
JuddRyan <ryanjudd03@gmail.com>
2023-12-15 17:42:56 +0100

    added color config

---
+++ src/config/colors.js
@@ -0,0 +1,17 @@
+const colors = {
+  background: {
+    dark: "#161616",
+    light: "#323232",
+  },
+  accent: "#981DF8",
+  text: {
+    light: "#fff", // Text on #161616, #323232, #981DF8 backgrounds
+    dark: "#000",
+  },
+  left: "#FF5733", // Color for left
+  right: "#33FF57", // Color for right
+  neutral: "#337AFF", // Color for neutral
+  bothHands: "#FF33E9", // Color for both hands
+};
+
+export default colors;
