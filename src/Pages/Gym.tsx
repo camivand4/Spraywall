ff0a50e68182b0c5a89919daae04ead37d979ad6
camivand4 <camivand4@student.arteveldehs.be>
2023-12-15 15:21:32 +0100

    fixed router

--- src/Pages/Gym.tsx
+++ src/Pages/Gym.tsx
@@ -1,5 +1,11 @@
+import { useParams } from "react-router-dom"
+
 export default function Gym() {
+
+    const { id } = useParams()
+
   return (
-    <div>Gym</div>
+    <div>Gym {id}</div>
   )
 }
+
--- src/Pages/Gyms.tsx
+++ src/Pages/Gyms.tsx
@@ -1,5 +1,12 @@
-export default function Gyms() {
+import { Link } from "react-router-dom";
+
+export default function () {
   return (
-    <div>Gyms</div>
+    <div>
+        <Link to="/gyms/1">Gym1</Link>
+        <Link to="/gyms/2">Gym2</Link>
+        <Link to="/gyms/3">Gym3</Link>
+
+    </div>
   )
 }
