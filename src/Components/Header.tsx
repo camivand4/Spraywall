32da479fd3ee469cbf25b70280dffb6df8167446
camivand4 <camivand4@student.arteveldehs.be>
2023-12-20 16:30:23 +0100

    hopefully centered

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,43 +1,65 @@
+import React from "react";
 import styled from "styled-components";
 import Colors from "../config/colors";
 import BackButton from "./BackButton";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const HiddenBack = styled.div `
+const HiddenBack = styled.div`
   width: 1.2rem;
 `;
 
-const StyledHeader = styled.header `
+interface StyledHeaderProps {
+  isHomePage: boolean;
+}
+
+const StyledHeader = styled.header<StyledHeaderProps>` 
   padding: 1rem 2rem;
   background-color: ${Colors.background.light};
   display: flex;
-  justify-content: space-between;
+  justify-content: ${({ isHomePage }) => (isHomePage ? "center" : "space-between")};
   align-items: center;
 
   h1 {
     font-size: 1.5rem;
     background: transparent;
     padding: 0 2rem;
-    /* Override link styles */
     color: inherit;
     text-decoration: none;
   }
 
-  /* Style the Link component */
   a {
     color: inherit;
     text-decoration: none;
   }
 `;
 
+const getPageTitle = (pathname: string) => {
+  switch (pathname) {
+    case "/":
+      return "Home";
+    case "/profile":
+      return "Profile";
+    case "/gyms":
+      return "Gyms";
+    case "/gym":
+      return "Gym";
+    default:
+      return "Page Not Found";
+  }
+};
+
 export default function Header() {
-    return (
-        <StyledHeader>
-            <BackButton></BackButton>
-            <Link to="/">
-                <h1>Header</h1>
-            </Link>
-            <HiddenBack></HiddenBack>
-        </StyledHeader>
-    );
+  const location = useLocation();
+  const pageTitle = getPageTitle(location.pathname);
+  const isHomePage = location.pathname === "/";
+
+  return (
+    <StyledHeader isHomePage={isHomePage}>
+      {!isHomePage && <BackButton />}
+      <Link to="/">
+        <h1>{pageTitle}</h1>
+      </Link>
+      {!isHomePage && <HiddenBack />}
+    </StyledHeader>
+  );
 }
