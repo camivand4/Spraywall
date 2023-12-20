ab4a88ee3f7a47fe85a50ff34eb6db7978d75c04
JuddRyan <ryanjudd03@gmail.com>
2023-12-20 15:21:59 +0100

    added minimal styling change to baselayout

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,14 +1,19 @@
+import styled from "styled-components";
 import Header from "../Components/Header";
 
 type Props = {
   children: JSX.Element;
 };
 
+const BaseLayoutStyle = styled.div`
+  padding-inline: 2rem;
+`;
+
 const BaseLayout = ({ children }: Props) => {
   return (
     <>
       <Header></Header>
-      {children}
+      <BaseLayoutStyle>{children}</BaseLayoutStyle>
     </>
   );
 };
