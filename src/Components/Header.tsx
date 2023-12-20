32c9fbde6a4a041769167e1027109512efb6a8a2
camivand4 <camivand4@student.arteveldehs.be>
2023-12-20 16:39:10 +0100

    router in home will delete later

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,21 +1,22 @@
 import styled from "styled-components";
 import Colors from "../config/colors";
 import BackButton from "./BackButton";
-import { Link, useLocation } from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
-const HiddenBack = styled.div`
+const HiddenBack = styled.div `
   width: 1.2rem;
 `;
 
 interface StyledHeaderProps {
-  isHomePage: boolean;
+    isHomePage : boolean;
 }
 
-const StyledHeader = styled.header<StyledHeaderProps>` 
+const StyledHeader = styled.header < StyledHeaderProps > ` 
   padding: 1rem 2rem;
   background-color: ${Colors.background.light};
   display: flex;
-  justify-content: ${({ isHomePage }) => (isHomePage ? "center" : "space-between")};
+  justify-content: ${ ({
+    isHomePage}) => (isHomePage ? "center" : "space-between")};
   align-items: center;
 
   h1 {
@@ -32,34 +33,37 @@ const StyledHeader = styled.header<StyledHeaderProps>`
   }
 `;
 
-const getPageTitle = (pathname: string) => {
-  switch (pathname) {
-    case "/":
-      return "Home";
-    case "/profile":
-      return "Profile";
-    case "/gyms":
-      return "Gyms";
-    case "/gym":
-      return "Gym";
-    default:
-      return "Page Not Found";
-  }
-};
+    const getPageTitle = (pathname : string) => {
+        switch (pathname) {
+            case "/":
+                return "Home";
+            case "/profile":
+                return "Profile";
+            case "/gyms":
+                return "Gyms";
+            case "/gym":
+                return "Gym";
+            case "/auth":
+                return "Auth";
+            case "/boulder":
+                return "Boulder";
+            default:
+                return "Page Not Found";
+        }
+    };
 
+    export default function Header() {
+        const location = useLocation();
+        const pageTitle = getPageTitle(location.pathname);
+        const isHomePage = location.pathname === "/";
 
-export default function Header() {
-  const location = useLocation();
-  const pageTitle = getPageTitle(location.pathname);
-  const isHomePage = location.pathname === "/";
-
-  return (
-    <StyledHeader isHomePage={isHomePage}>
-      {!isHomePage && <BackButton />}
-      <Link to="/">
-        <h1>{pageTitle}</h1>
-      </Link>
-      {!isHomePage && <HiddenBack />}
-    </StyledHeader>
-  );
-}
+        return (
+            <StyledHeader isHomePage={isHomePage}>
+                {!isHomePage && <BackButton/>}
+                <Link to="/">
+                    <h1>{pageTitle}</h1>
+                </Link>
+                {!isHomePage && <HiddenBack/>}
+            </StyledHeader>
+        );
+    }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom";
 import Button from "../Components/Button";
 
 export default function Home() {
   return (
     <>
       <Button>send</Button>
+      <br />
+      <Link to="profile">Profile</Link>
+      <br />
+      <Link to="gyms">gyms</Link>
+      <br />
+      <Link to="gym">gym</Link>
+      <br />
+      <Link to="auth">auth</Link>
+      <br />
+      <Link to="boulder">boulder</Link>
     </>
   );
 }
