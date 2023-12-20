e9894459f7ea45216c79c25b8cb4a08ce576f731
JuddRyan <ryanjudd03@gmail.com>
2023-12-20 16:35:45 +0100

    added simple data file to loop over in gyms page

--- src/Pages/Gyms.tsx
+++ src/Pages/Gyms.tsx
@@ -1,44 +1,18 @@
 import List from "../Components/List";
 import ListItem from "../Components/ListItem";
 
+import testGyms from "../config/testGyms";
+
 export default function Gyms() {
   return (
     <List>
-      <ListItem
-        sectionLeft="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.klimenbergsportfederatie.be%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Ffacebook%2Fpublic%2Fgenerated%2Fimages%2Fclimbing%2FLogo%2520Bleau%2520Full%2520Color-01.png%3Fitok%3D3E6CiGO1&f=1&nofb=1&ipt=76abe20a794292a98d79207fe6c781360c19ecbc6786b4e5aff2fc180f5f8a4e&ipo=images"
-        text="Gym1"
-        to="/gyms/1"
-      />
-      <ListItem sectionLeft="7a" text="Gym 2" to="/gyms/2" />
-      <ListItem sectionLeft="7a" text="Gym3" to="/gyms/3" />
-      <ListItem
-        sectionLeft="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.klimenbergsportfederatie.be%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Ffacebook%2Fpublic%2Fgenerated%2Fimages%2Fclimbing%2FLogo%2520Bleau%2520Full%2520Color-01.png%3Fitok%3D3E6CiGO1&f=1&nofb=1&ipt=76abe20a794292a98d79207fe6c781360c19ecbc6786b4e5aff2fc180f5f8a4e&ipo=images"
-        text="Gym1"
-        to="/gyms/1"
-      />
-      <ListItem sectionLeft="7a" text="Gym 2" to="/gyms/2" />
-      <ListItem sectionLeft="7a" text="Gym3" to="/gyms/3" />
-      <ListItem
-        sectionLeft="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.klimenbergsportfederatie.be%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Ffacebook%2Fpublic%2Fgenerated%2Fimages%2Fclimbing%2FLogo%2520Bleau%2520Full%2520Color-01.png%3Fitok%3D3E6CiGO1&f=1&nofb=1&ipt=76abe20a794292a98d79207fe6c781360c19ecbc6786b4e5aff2fc180f5f8a4e&ipo=images"
-        text="Gym1"
-        to="/gyms/1"
-      />
-      <ListItem sectionLeft="7a" text="Gym 2" to="/gyms/2" />
-      <ListItem sectionLeft="7a" text="Gym3" to="/gyms/3" />
-      <ListItem
-        sectionLeft="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.klimenbergsportfederatie.be%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Ffacebook%2Fpublic%2Fgenerated%2Fimages%2Fclimbing%2FLogo%2520Bleau%2520Full%2520Color-01.png%3Fitok%3D3E6CiGO1&f=1&nofb=1&ipt=76abe20a794292a98d79207fe6c781360c19ecbc6786b4e5aff2fc180f5f8a4e&ipo=images"
-        text="Gym1"
-        to="/gyms/1"
-      />
-      <ListItem sectionLeft="7a" text="Gym 2" to="/gyms/2" />
-      <ListItem sectionLeft="7a" text="Gym3" to="/gyms/3" />
-      <ListItem
-        sectionLeft="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.klimenbergsportfederatie.be%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Ffacebook%2Fpublic%2Fgenerated%2Fimages%2Fclimbing%2FLogo%2520Bleau%2520Full%2520Color-01.png%3Fitok%3D3E6CiGO1&f=1&nofb=1&ipt=76abe20a794292a98d79207fe6c781360c19ecbc6786b4e5aff2fc180f5f8a4e&ipo=images"
-        text="Gym1"
-        to="/gyms/1"
-      />
-      <ListItem sectionLeft="7a" text="Gym 2" to="/gyms/2" />
-      <ListItem sectionLeft="7a" text="Gym3" to="/gyms/3" />
+      {testGyms.map((i) => (
+        <ListItem
+          sectionLeft={i.image}
+          text={i.name}
+          to={i.id.toString()}
+        ></ListItem>
+      ))}
     </List>
   );
 }
