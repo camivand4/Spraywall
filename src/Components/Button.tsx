8b4005dac4412ac9c542811afe1431636da9bbf4
camivand4 <camivand4@student.arteveldehs.be>
2024-01-09 17:50:57 +0100

    lelijke boulder page met button changes maar moet nog over nagedacht worden

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -7,11 +7,13 @@ type ButtonProps = {
   height?: string;
   width?: string;
   radius?: string;
+  border?: string;
+  fontWeight?: string;
 };
 
 const Button = styled.button<ButtonProps>`
   border-radius: ${(props) => props.radius || "1rem"};
-  border: none;
+  border: ${(props) => (props.border ? `solid 2px ${props.border}` : "none")};
   background-color: ${(props) =>
     props.backgroundColor || Colors.background.white};
   color: ${(props) => props.color || null};
@@ -20,6 +22,8 @@ const Button = styled.button<ButtonProps>`
   height: ${(props) => props.height || "3rem"};
 
   font-size: 1.4rem;
+  font-weight: ${(props) => props.fontWeight || null};
+  padding: 0.5rem 1rem;
 `;
 
 export default Button;
--- src/Pages/Boulder.tsx
+++ src/Pages/Boulder.tsx
@@ -1,14 +1,25 @@
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import styled from "styled-components";
+import Button from "../Components/Button";
+import Colors from "../config/colors";
 
 const ImageContainer = styled.div`
   display: flex;
   justify-content: center;
+  padding-top: 2rem;
+`;
+
+const ButtonContainer = styled.div`
+  position: absolute;
+  display: flex;
+  bottom: 1rem;
+  gap: 0.5rem;
 `;
 
 const Image = styled.img`
-  width: 100%;
-  max-height: 95vh;
+  max-width: 100vw;
+  max-height: 85vh;
+  border-radius: 5px;
 `;
 
 export default function Boulder() {
@@ -19,6 +30,13 @@ export default function Boulder() {
           <Image src="https://i.ibb.co/0BBrmFJ/realcliumb.jpg" alt="spraywall image" />
         </TransformComponent>
       </TransformWrapper>
+      <ButtonContainer>
+        <Button width="20vw" backgroundColor={Colors.accent} color={Colors.text.light}>Left</Button>
+        <Button width="20vw" backgroundColor={Colors.accent} color={Colors.text.light}>Right</Button>
+        <Button width="20vw" backgroundColor={Colors.accent} color={Colors.text.light}>Neutral</Button>
+        <Button width="20vw" backgroundColor={Colors.accent} color={Colors.text.light}>Both</Button>
+
+      </ButtonContainer>
     </ImageContainer>
   );
 }
